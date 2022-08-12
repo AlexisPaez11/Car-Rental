0a4d8b775672ba1aa5293498226af47da497243b
Alexis Paez <alexispaez280103@gmail.com>
2022-08-11 21:06:09 -0300

    menu button - css add

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink} from "react-router-dom";
+import { HiMenuAlt3 } from 'react-icons/hi'
 
 const Nav = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
   return (
     <nav>
-        <NavLink to="/" className='logo'>
-          <span>OPTIMUS</span>
+      <NavLink to="/" className='logo'>
+        <span>OPTIMUS</span>
+      </NavLink>
+
+      <ul className={isMobile ? 'nav-menu-mobile' : 'nav-menu'} onClick={() => setIsMobile(false)}>
+        <li className="nav-menu-item">
+        <NavLink to="/">Home</NavLink></li>
+        
+        <li className="nav-menu-item">
+        <NavLink to="/Catalogue">Catalogue</NavLink></li>
+
+        <li className="nav-menu-item">
+        <NavLink to="/About">About Us</NavLink></li>
+        
+        <li className="nav-menu-item">
+        <NavLink to="/Help">Help</NavLink></li>
+
+        <NavLink to="#">
+            <div className='btn'>Download App</div>
         </NavLink>
-
-        <ul>
-            <li className="nav-menu-item">
-            <NavLink to="/">Home</NavLink></li>
-            
-            <li className="nav-menu-item">
-            <NavLink to="/Catalogue">Catalogue</NavLink></li>
-
-            <li className="nav-menu-item">
-            <NavLink to="/About">About Us</NavLink></li>
-            
-            <li className="nav-menu-item">
-            <NavLink to="/Help">Help</NavLink></li>
-
-            <NavLink to="#">
-                <div className='btn'>Download App</div>
-            </NavLink>
-        </ul>
+      </ul>
+        <button className='menu' onClick={() => setIsMobile(!isMobile)}>
+          {isMobile ? <HiMenuAlt3 /> : <HiMenuAlt3 />}
+        </button>
     </nav>
   )
 }
